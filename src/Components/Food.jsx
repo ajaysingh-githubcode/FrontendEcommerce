4aa0a15ef2470e0c67c075ef9bb60258a3c3e049
AJAY SINGH <shareajay84@gmail.com>
2024-10-03 00:48:58 +0530

    Update Food.jsx

--- src/Components/Food.jsx
+++ src/Components/Food.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Food = () => {
   return (
-    <div>This is another venture of Just buy</div>
+    <div className="bg-cyan-500 py-4 px-4">This is another venture of Just buy</div>
   )
 }
 
